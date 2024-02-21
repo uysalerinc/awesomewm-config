7693c708b8a72e39324b03efe84c52a5ab0ec78b
uysalerinc <erincuysal@outlook.com>
2024-02-21 17:46:59 +0300

    appkeys update

--- keys/appkeys.lua
+++ keys/appkeys.lua
@@ -7,6 +7,17 @@ appkeys = gtable.join(
     function()
       awful.spawn("firefox")
     end,
-    {description = "open Firefox", group = "apps"})
+    {description = "open Firefox", group = "apps"}),
 
+  awful.key({ }, "Print",
+    function()
+      awful.spawn.with_shell("screenshot -s")
+    end,
+    {description = "Take screenshot of area", group = "apps"}),
+
+  awful.key({shiftkey }, "Print",
+    function()
+      awful.spawn.with_shell("screenshot")
+    end,
+    {description = "Take screenshot entire screen", group = "apps"})
   )
